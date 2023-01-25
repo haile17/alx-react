66b2e20266aeaf8d053b9236482cb0ef1c526f87
Haile Fikere <hailefikere17@gmail.com>
2023-01-25 11:46:16 +0300

    task 2 update

--- 0x01-react_intro/task_2/dashboard/src/utils.js
+++ 0x01-react_intro/task_2/dashboard/src/utils.js
@@ -1,17 +1,15 @@
-function getFullYear() {
+export const getFullYear = () => {
     return new Date().getFullYear();
 }
 
-function getFooterCopy(isIndex) {
+export const  getFooterCopy = (isIndex) => {
     if (isIndex === true){
         return 'Holberton School';
     } else if (isIndex === false) {
         return 'Holberton School main dashboard'
     }
 }
+export const  getLatestNotification = () => 
+    '<strong>Urgent requirement</strong> - complete by EOD'
 
-function getLatestNotification() {
-    return '<strong>Urgent requirement</strong> - complete by EOD'
-}
 
-export { getFullYear, getFooterCopy}
